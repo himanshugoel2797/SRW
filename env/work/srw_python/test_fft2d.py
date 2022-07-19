2207d4b7d08884485b6b3eeb33796e76681c961d
Himanshu Goel <himanshu@hgoel.dev>
2022-07-19 18:27:13 -0400

    Update test_fft2d to be more fair.

--- env/work/srw_python/test_fft2d.py
+++ env/work/srw_python/test_fft2d.py
@@ -6,13 +6,6 @@
 from __future__ import print_function  # Python 2.7 compatibility
 from srwlib import *
 from uti_plot import *
-import os
-
-import ctypes
-import cupy as cp
-import numpy as np
-#import cupy.cuda.memory
-#cp.cuda.set_allocator(cupy.cuda.memory.malloc_managed)
 
 
 #**********************Input Parameters:
@@ -26,10 +19,9 @@ xStep = xRange/(xNp)
 
 print ('gpuEn =', gpuEn)
 
-ar_cp = array('f', [0]*(xNp*xNp*2))
-ar = ar_cp
 
 for _ in range(10):
+    ar = array('f', [0]*(xNp*xNp*2))
     y = xStart
     for i in range(xNp):
         x = xStart
@@ -42,11 +34,11 @@ for _ in range(10):
 
     mesh = [xStart, xStep, xNp, xStart, xStep, xNp]
 
-    ar_Re = array('f', [0]*(xNp*xNp))
-    ar_Im = array('f', [0]*(xNp*xNp))
-    for i in range(xNp*xNp):
-        ar_Re[i] = ar[2*i]
-        ar_Im[i] = ar[2*i + 1]
+    #ar_Re = array('f', [0]*(xNp*xNp))
+    #ar_Im = array('f', [0]*(xNp*xNp))
+    #for i in range(xNp*xNp):
+    #    ar_Re[i] = ar[2*i]
+    #    ar_Im[i] = ar[2*i + 1]
     #uti_plot2d(ar_Re, [mesh[0], mesh[0] + mesh[1]*xNp, xNp], [mesh[3], mesh[3] + mesh[4]*xNp, xNp], ['Qx', 'Qy', 'Input'])
     #input('Waiting for enter.')
 
@@ -55,11 +47,11 @@ for _ in range(10):
     else:
         srwl.UtiFFT(ar, mesh, 1, 0)
 
-    arFT_Re = array('f', [0]*(xNp*xNp))
-    arFT_Im = array('f', [0]*(xNp*xNp))
-    for i in range(xNp*xNp):
-        arFT_Re[i] = ar[2*i]
-        arFT_Im[i] = ar[2*i + 1]
+    #arFT_Re = array('f', [0]*(xNp*xNp))
+    #arFT_Im = array('f', [0]*(xNp*xNp))
+    #for i in range(xNp*xNp):
+    #    arFT_Re[i] = ar[2*i]
+    #    arFT_Im[i] = ar[2*i + 1]
         #print(arFT_Re[i], arFT_Im[i])
 
 #uti_plot1d(arFT_Re, [mesh[0], mesh[0] + mesh[1]*xNp, xNp],
